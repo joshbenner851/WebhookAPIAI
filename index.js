d4b231be7e91da5326dd95c79073814ac6bc4e95
Josh Benner <bennerster@gmail.com>
2017-11-27 21:39:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,43 +16,43 @@ app.use( bodyParser.json() );
 
 
 app.post( "/growthchart", function( req, res ) {
-    if( req.body.result.action == "LogObservation" ) {
-        const student_name = req.body.result.parameters.studentName;
-        const category = req.body.result.parameters.category;
-        const event = req.body.result.parameters.event;
+//     if( req.body.result.action == "LogObservationAction" ) {
+//         const student_name = req.body.result.parameters.studentName;
+//         const category = req.body.result.parameters.category;
+//         const event = req.body.result.parameters.event;
         
-        const observation = {
-              name: [student_name],
-              category: [category], 
-              event: [event]
-        }
+//         const observation = {
+//               name: [student_name],
+//               category: [category], 
+//               event: [event]
+//         }
         
-        request.post(
-            "http://dev.api.growthchart.me/observation/" + student_name,
-            { json: { observation } },
-            function( error, response ) {
-                if( !error && response.statusCode == 200 ) {
-                    return res.json( {
-                        speech: response.body.question,
-                        displayText: response.body.question,
-                    } );
-                } else {
-                    return res.json( {
-                        speech: "posted but fuck",
-                        displayText: "posted but fuck",
-                    } );
-                }
-            }
-        );
-    }
+//         request.post(
+//             "http://dev.api.growthchart.me/observation/" + student_name,
+//             { json: { observation } },
+//             function( error, response ) {
+//                 if( !error && response.statusCode == 200 ) {
+//                     return res.json( {
+//                         speech: response.body.question,
+//                         displayText: response.body.question,
+//                     } );
+//                 } else {
+//                     return res.json( {
+//                         speech: "posted but fuck",
+//                         displayText: "posted but fuck",
+//                     } );
+//                 }
+//             }
+//         );
+//     }
     
-    else {
+//     else {
         return res.json( {
             speech: "Grr",
             displayText: "Grrr",
            
         } );
-    }
+//     }
 } );
 
 app.listen( (process.env.PORT || 8000), function() {
